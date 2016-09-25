c90ea474666930af15e225d94903567ee8e3fa3c
av <av@fun-box.ru>
2016-09-25 19:18:53 +0300

    Updated deployment scripts && production configs

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 RELEASE=$1
+BRANCH=$2
 
 if [ "x$RELEASE" == "x" ]
 then
@@ -10,11 +11,17 @@ then
     exit 1
 fi
 
+if [ "x$BRANCH" == "x" ]
+then
+    echo "Branch name not set"
+    exit 1
+fi
+
 RELEASE_DIR=releases/$1
 
 if ! [ -d $RELEASE_DIR ]
 then
-    ./release.sh $RELEASE
+    ./release.sh $RELEASE $BRANCH
 fi
 
 ansible-playbook -i deploy/hosts deploy/deploy.yml --extra-vars "release_dir=$RELEASE_DIR"
