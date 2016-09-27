da8a0ca84991f508db6ab811348ca06d9ca02fa0
av <av@fun-box.ru>
2016-09-27 12:25:01 +0300

    Added Copyright note

--- web/static/js/container.js
+++ web/static/js/container.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React from "react"
 import {SystemIds} from "./system_ids"
 import {History} from "./history"
@@ -21,8 +22,28 @@ export class Container extends React.Component {
                 <div key={this.state.selected}>
                     <History systemId={this.state.selected}/>
                 </div>
+                {this.copy()}
             </div>
         );
+    }
+
+    copy() {
+        return (
+            <footer>
+                <p className="text-center text-muted">
+                    <a href="https://github.com/savonarola/smppex">SMPPEX</a>
+                    <span> is released under the </span>
+                    <a href="https://opensource.org/licenses/MIT">MIT License</a>
+                    <span> - Copyright &copy; </span>
+                    {this.year()}
+                </p>
+            </footer>
+        );
+    }
+
+    year() {
+        let time = moment();
+        return time.format("YYYY");
     }
 
 }
